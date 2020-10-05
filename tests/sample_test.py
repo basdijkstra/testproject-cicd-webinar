a0e6221668e7df852a433f4685df600fc218192e
Bas Dijkstra <bas@ontestautomation.com>
2020-10-05 09:28:54 +0200

    Initial commit with sample tests

---
+++ tests/sample_test.py
@@ -0,0 +1,15 @@
+from src.testproject.sdk.drivers import webdriver
+
+
+def test_sample():
+    driver = webdriver.Chrome()
+
+    driver.get("https://example.testproject.io/web/")
+
+    driver.find_element_by_css_selector("#name").send_keys("John Smith")
+    driver.find_element_by_css_selector("#password").send_keys("12345")
+    driver.find_element_by_css_selector("#login").click()
+
+    result = driver.find_element_by_css_selector("#greetings").is_displayed()
+
+    assert result is False
